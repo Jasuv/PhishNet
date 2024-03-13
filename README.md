66f9a9732fdbfee3da910332ce035d65106903fe
unknown <ejahsue@gmail.com>
2024-03-13 09:47:37 -0700

    v.1.0

---
+++ README.md
@@ -0,0 +1,124 @@
+Everything is based on this blog post by Samson Zhang: https://www.samsonzhang.com/2020/11/24/understanding-the-math-behind-neural-networks-by-building-one-from-scratch-no-tf-keras-just-numpy
+
+-----forward propagation-----
+
+take input and run it though the network
+
+a = activated layers
+z = unactivated layers
+w = weight (dotproduct between a[0] to matrix)
+b = bias (constant)
+g = activation function
+
+# start at the input layer
+a = x
+# calculate unactivated first layer 
+z1 = w1 * a + b1
+# apply activation to layer
+a1 = g(z1)
+# calculate unactivated second layer
+z2 = w2 * a1 + b2
+# apply softmax to layer
+a2 = softmax(z2)
+
+activation function: makes the model non-linear and introduces 
+complex patterns to the algorithm (tanh, sigmoid, or ReLU)
+
+Rectified Linear Unit(ReLU): x if x > 0 | 0 if x <= 0
+
+softmax activation function: https://docs-assets.developer.apple.com/published/c2185dfdcf/0ab139bc-3ff6-49d2-8b36-dcc98ef31102.png
+scales all values to be between 0 to 1
+
+-----backwards propagation-----
+
+opposite way, start with prediction then
+determine how much the result has deviated from it.
+one-hot encoding ensures that model does not assume 
+that higher numbers are more important.
+
+# how much we were off
+dz = error of the layer
+# how much of the error is from the weights
+dw = derivative of the loss function with respect with to the weight
+# how much of the error is from the biases
+db = average of the absolute error
+y = label
+g' = undo actiation function
+
+dz[2] = a[2] - y
+dw[2] = (1/m)dz[2]a[1]
+db[2] = (1/m)sum{}(dz[2])
+dz[1] = w[2]dz[2]g'(z[1])
+
+-----update parameters-----
+
+updates the initial parameters with newer refined ones
+
+alpha = learning rate (manually set)
+
+w[1] = w[1] - alpha * dw[1]
+b[1] = b[1] - alpha * db[1]
+w[2] = w[2] - alpha * dw[2]
+b[2] = b[2] - alpha * db[2]
+
+-----the general idea-----
+
+forward porpagation -> backward porpagation -> update parameters -> repeat
+
+-----testing-----
+
+Epoch: 0
+Prediction: [0 0 0 ... 0 0 0] Y: [1 1 0 ... 1 1 0]
+Accuracy: 0.28478460712302833
+
+Epoch: 10
+Prediction: [1 1 1 ... 0 0 0] Y: [1 1 0 ... 1 1 0]
+Accuracy: 0.36878691896713356
+
+Epoch: 20
+Prediction: [1 1 1 ... 0 1 0] Y: [1 1 0 ... 1 1 0]
+Accuracy: 0.5538300157460249
+
+Epoch: 30
+Prediction: [1 1 1 ... 0 1 0] Y: [1 1 0 ... 1 1 0]
+Accuracy: 0.5914884999408386
+
+Epoch: 40
+Prediction: [1 1 1 ... 0 1 1] Y: [1 1 0 ... 1 1 0]
+Accuracy: 0.6069478196761597
+
+Epoch: 50
+Prediction: [1 1 1 ... 0 1 1] Y: [1 1 0 ... 1 1 0]
+Accuracy: 0.6083927222419426
+
+...
+
+Epoch: 840
+Prediction: [1 1 0 ... 1 1 1] Y: [1 1 0 ... 1 1 0]
+Accuracy: 0.7574930144080678
+
+Epoch: 850
+Prediction: [1 1 0 ... 1 1 1] Y: [1 1 0 ... 1 1 0]
+Accuracy: 0.7575590020843004
+
+Epoch: 860
+Prediction: [1 1 0 ... 1 1 1] Y: [1 1 0 ... 1 1 0]
+Accuracy: 0.7576704985027624
+
+Epoch: 870
+Prediction: [1 1 0 ... 1 1 1] Y: [1 1 0 ... 1 1 0]
+Accuracy: 0.7577637914243326
+
+Epoch: 880
+Prediction: [1 1 0 ... 1 1 1] Y: [1 1 0 ... 1 1 0]
+Accuracy: 0.7578616352201258
+
+Epoch: 890
+Prediction: [1 1 0 ... 1 1 1] Y: [1 1 0 ... 1 1 0]
+Accuracy: 0.758109657865276
+
+---plans---
+
+- add make_prediction
+- optimize for faster learning & better acc
+- save model to file like .pt
--- phishnet.py
+++ phishnet.py
@@ -1,41 +1,47 @@
+'''
+Based heavily off of SAMSON ZHANG's simple neural network
+https://www.kaggle.com/code/wwsalmon/simple-mnist-nn-from-scratch-numpy-no-tf-keras
+'''
+
 import numpy as np
 import pandas as pd
 
-# rectified linear unit activation function
-def ReLU(z):
-    return np.maximum(0, z)
+# activation function
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
-# undo ReLU function
-def ReLU_prime(z):
-    return z > 0
+# undo activation function
+def sigmoid_prime(x):
+    return sigmoid(x) * (1 - sigmoid(x))
 
 # softmax activation function
 def softmax(z):
-    z = z - np.max(z, 0)
-    a = np.exp(z) / np.sum(np.exp(z), 0)
-    return a
+    z_exp = np.exp(z - np.max(z, axis=1, keepdims=True))
+    return z_exp / np.sum(z_exp, axis=1, keepdims=True) 
+
+# one-hot encoding
+def one_hot(y):
+    return np.eye(2)[y]
 
 # forward propagation
 def forward_prop(w1, b1, w2, b2, x):
+    # input to hidden
     z1 = np.dot(x, w1) + b1
-    a1 = ReLU(z1)
+    a1 = sigmoid(z1)
+    # hidden to output
     z2 = np.dot(a1, w2) + b2
     a2 = softmax(z2)
     return z1, a1, z2, a2
 
-# one-hot encoding
-def one_hot(y):
-    one_hot_y = np.zeros((y.size, y.max() + 1))
-    one_hot_y[np.arange(y.size), y] = 1
-    return one_hot_y
-
 # backward propagation
 def backward_prop(z1, a1, z2, a2, w2, x, y):
     m = y.shape[0]
+    # output to hidden
     dz2 = a2 - one_hot(y)
     dw2 = (1 / m) * np.dot(a1.T, dz2)
     db2 = (1 / m) * np.sum(dz2, axis=0)
-    dz1 = np.dot(dz2, w2.T) * ReLU_prime(z1)
+    # hidden to input
+    dz1 = np.dot(dz2, w2.T) * sigmoid_prime(z1)
     dw1 = (1 / m) * np.dot(x.T, dz1)
     db1 = (1 / m) * np.sum(dz1, axis=0)
     return dw1, db1, dw2, db2
@@ -49,15 +55,13 @@ def update_params(w1, b1, w2, b2, dw1, db1, dw2, db2, alpha):
     return w1, b1, w2, b2
 
 def get_predictions(a2):
-    return np.argmax(a2, 0)
+    return np.argmax(a2, 1)
 
 def get_accuracy(predictions, y):
-    print(predictions, y)
-    return np.sum(predictions == y) / y.size
+    return np.mean(predictions == y)
 
 # train model using gradient descent
 def gradient_descent(x, y, epochs, alpha):
-    np.random.seed(42)
     # initialize weights and biases with random numbers
     input_size = x.shape[1]
     hidden_size = 128 
@@ -71,42 +75,35 @@ def gradient_descent(x, y, epochs, alpha):
         w1, b1, w2, b2 = update_params(w1, b1, w2, b2, dw1, db1, dw2, db2, alpha)
         # log every 10 results
         if epoch % 10 == 0:
-            print(f"Iteration: {epoch}")
-            print(f"Accuracy: {get_accuracy(get_predictions(a2), y)}")
+            pre = get_predictions(a2)
+            acc = get_accuracy(pre, y)
+            print(f"Epoch: {epoch}\nPrediction: {pre} Y: {y}\nAccuracy: {acc}\n")
     return w1, b1, w2, b2
 
-dataset = pd.read_csv("phishing_site_urls.csv")
-np.random.shuffle(dataset)
+dataset = pd.read_csv("urls.csv")
+dataset = dataset.sample(frac=1).reset_index(drop=True)
 
-features = ['-', 'confirm', 'account', 'signin', 'update', 'logon', 
-            'cmd', 'admin', '.', '!', '&', ',', '#', '$', '%', '@']
-
-# extract features from URL
+# Create dataset (features, binary label)
+features = ['confirm', 'account', 'signin', 'update', 'login', 'cmd', 'cmnd', 'admin', 'post', 'upload', 
+            'exe', 'ph', 'js', 'css', 'my', '/', '_', '-', '.', '?', '!', '&', ',', '#', '$', '%', '@']
 def extract_features(url):
-    feature_count = [url.count(feature) for feature in features]
-    return np.array(feature_count)
-x = np.array(extract_features(url) for url in dataset['URL'])
-y = np.array(dataset['Label'])
+    return np.array([url.count(feature) for feature in features])
+def encode_labels(y):
+    return np.array([1 if label == 'good' else 0 for label in y])
+x = np.array([extract_features(url) for url in dataset['URL']])
+y = encode_labels(dataset['Label'])
 
 # split dataset into train and test sets
 split_ratio = 0.8
 split_index = int(len(dataset) * split_ratio)
+x_url = dataset['URL']
 x_train, x_test = x[:split_index], x[split_index:]
 y_train, y_test = y[:split_index], y[split_index:]
 
+'''
+for i in range(len(dataset)):
+    print(f"URL: {x_url[i]}\nx: {x_train[i]}\ny: {y_train[i]}\n\n")
+'''
+    
 # begin training
 w1, b1, w2, b2 = gradient_descent(x_train, y_train, epochs=1000, alpha=0.01)
-
-def make_predictions(x, w1, b1, w2, b2):
-    a2 = forward_prop(w1, b1, w2, b2, x)
-    predictions = get_predictions(a2)
-    return predictions
-
-def test_prediction(index, w1, b1, w2, b2):
-    current_image = x_train[:, index, None]
-    prediction = make_predictions(x_train[:, index, None], w1, b1, w2, b2)
-    label = y_train[index]
-    print("Prediction: ", prediction)
-    print("Label: ", label)
-
-test_prediction(0, w1, b1, w2, b2)
